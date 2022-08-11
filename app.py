4511eb603d8d89ba292d385bb1f62deaffc54569
Simon <simonbastas7@gmail.com>
2022-08-11 21:38:00 +0200

    Fixed Heroku postgres connection

--- app.py
+++ app.py
@@ -13,11 +13,7 @@ from security import authenticate, identity
 
 app = Flask(__name__)
 
-uri = os.getenv("DATABASE_URL")  # or other relevant config var
-if uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URI",'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'simon'
 api = Api(app)
