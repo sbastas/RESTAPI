381334fc601e4d4db5a5e2865275af91ed76dfdc
Simon <simonbastas7@gmail.com>
2022-08-11 21:36:34 +0200

    postgres issue

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ uri = os.getenv("DATABASE_URL")  # or other relevant config var
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(uri)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'simon'
 api = Api(app)
